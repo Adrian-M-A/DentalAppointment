5414f3c32530fb132b6e9671b89f4e8db79d80a5
Adrian M.A <adrian@neurocadi.es>
2020-07-30 16:56:01 +0200

    Working environment ready

---
+++ dentalapp/src/containers/Landing/Landing.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Landing.css';
+
+class Landing extends React.Component {
+
+
+    render(){
+        return (
+            <div className="section" id="body">
+                <div id="first">
+                    <img src="" id="logoLanding" alt="Imagen del logo"/>
+                </div>
+                <div className="section" id="second">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Landing;
